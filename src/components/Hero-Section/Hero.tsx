0c940b5f9c535d1734716526690c391cb001cd8a
Reaper-187 <Abdulkader_cheik@outlook.de>
2025-12-19 10:38:10 +0100

    Hero-Section: - responsive-design: edit font-size for h1 and intro-text - remove tailwind-css from img to media queries better controll of img-width

--- src/components/Hero-Section/Hero.tsx
+++ src/components/Hero-Section/Hero.tsx
@@ -1,33 +1,33 @@
 import { FileDown } from "lucide-react";
 import { SocialMedia } from "../social-media/SocialMedia";
-import "./Hero.sass";
 import { Button } from "../ui/button";
 import socialMediaImg from "../../assets/Cheik_SocialMedia.jpg";
+import "./Hero.sass";
 
 const Intro = () => {
   return (
-    <div>
-      <div className="text-popover space-y-10">
-        <h1 className="text-5xl font-[calistoga]">hi Abdul here. 👋</h1>
+    <div className="space-y-10">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-[calistoga]">
+        hi Abdul here. 👋
+      </h1>
 
-        <div className="text-lg space-y-10">
-          <p>
-            Junior React Developer with a strong focus on frontend, excited by
-            full-stack.
-          </p>
-          <p>
-            My skills are hands-on and project-based. <br /> I aim to build real
-            products in a startup environment and keep growing.
-          </p>
-        </div>
+      <div className="text-sm md:text-base lg:text-lg space-y-10">
+        <p>
+          Junior React Developer with a strong focus on frontend, excited by
+          full-stack.
+        </p>
+        <p>
+          My skills are hands-on and project-based. <br /> I aim to build real
+          products in a startup environment and keep growing.
+        </p>
+      </div>
 
-        <div className="flex gap-5">
-          <Button className="cursor-pointer">
-            Resumé
-            <FileDown />
-          </Button>
-          <SocialMedia />
-        </div>
+      <div className="flex gap-5">
+        <Button className="cursor-pointer">
+          Resumé
+          <FileDown />
+        </Button>
+        <SocialMedia />
       </div>
     </div>
   );
@@ -37,12 +37,8 @@ export const Hero = () => {
   return (
     <section className="hero-wrapper">
       <Intro />
-      <div className="min-w-1/7 max-w-1/6">
-        <img
-          className="bg-red-500 rounded-lg"
-          src={socialMediaImg}
-          alt="Profile-Img"
-        />
+      <div className="hero-image">
+        <img src={socialMediaImg} alt="Profile-Img" />
       </div>
     </section>
   );
